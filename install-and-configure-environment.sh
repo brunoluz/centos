6d6d428c712640a34fb47024d524d7697b83a7c2
brunoluz <bru_luz69@hotmail.com>
2020-08-31 19:59:32 -0300

    Update install-and-configure-environment.sh

--- install-and-configure-environment.sh
+++ install-and-configure-environment.sh
@@ -6,3 +6,8 @@ sudo yum install xclip -y
 
 ## show full path on nautilus
 gsettings set org.gnome.nautilus.preferences always-use-location-entry true
+
+## install chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+sudo yum localinstall google-chrome-stable_current_x86_64.rpm -y
+rm -f google-chrome-stable_current_x86_64.rpm*
