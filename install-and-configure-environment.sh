d42097eae46d98ef3af312f6dc355ac48a81cc5a
brunoluz <bru_luz69@hotmail.com>
2020-08-31 21:40:00 -0300

    Update install-and-configure-environment.sh

--- install-and-configure-environment.sh
+++ install-and-configure-environment.sh
@@ -8,9 +8,7 @@ sudo dnf install git
 gsettings set org.gnome.nautilus.preferences always-use-location-entry true
 
 ## install chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
-sudo yum localinstall google-chrome-stable_current_x86_64.rpm -y
-rm -f google-chrome-stable_current_x86_64.rpm*
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm && sudo dnf localinstall google-chrome-stable_current_x86_64.rpm -y && rm -f google-chrome-stable_current_x86_64.rpm*
 
 # update grub
 sudo grub2-mkconfig --output=/boot/grub2/grub.cfg
