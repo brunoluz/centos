154aacff42e4d73f04c83ce7cf9e714359c40c50
brunoluz <bru_luz69@hotmail.com>
2020-08-31 21:38:21 -0300

    Update install-and-configure-environment.sh

--- install-and-configure-environment.sh
+++ install-and-configure-environment.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-sudo yum install epel-release -y
-sudo yum install xclip -y
-sudo yum install ntfs-3g
-
-# configure environment
+sudo dnf install epel-release -y
+sudo dnf install xclip -y
+sudo dnf install ntfs-3g
+sudo dnf install git
 
 ## show full path on nautilus
 gsettings set org.gnome.nautilus.preferences always-use-location-entry true
@@ -12,3 +11,6 @@ gsettings set org.gnome.nautilus.preferences always-use-location-entry true
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
 sudo yum localinstall google-chrome-stable_current_x86_64.rpm -y
 rm -f google-chrome-stable_current_x86_64.rpm*
+
+# update grub
+sudo grub2-mkconfig --output=/boot/grub2/grub.cfg
