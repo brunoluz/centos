37d1e33db343d9068de3389e6d6a09bede9c1639
brunoluz <bru_luz69@hotmail.com>
2020-09-01 22:15:04 -0300

    Update install-and-configure-environment.sh

--- install-and-configure-environment.sh
+++ install-and-configure-environment.sh
@@ -7,6 +7,9 @@ sudo dnf install git
 ## show full path on nautilus
 gsettings set org.gnome.nautilus.preferences always-use-location-entry true
 
+# nautilus - add "New Document" or right click
+touch ~/Templates/Empty\ Document
+
 ## install chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm && sudo dnf localinstall google-chrome-stable_current_x86_64.rpm -y && rm -f google-chrome-stable_current_x86_64.rpm*
 
