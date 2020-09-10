38717d17be35bd0f27f5a662f0465aeaff8c15ce
Bruno Luz <bruno.al.luz@gmail.com>
2020-09-09 21:30:49 -0300

    install vscode

--- install-and-configure-environment.sh
+++ install-and-configure-environment.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 sudo yum install epel-release -y
 sudo yum install xclip -y
+sudo dnf install chrome-gnome-shell -y
 
-# configure environment
+# tap to click
+gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
 
-## show full path on nautilus
-gsettings set org.gnome.nautilus.preferences always-use-location-entry true
+# install visual studio code
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo dnf check-update
+sudo dnf install code -y
